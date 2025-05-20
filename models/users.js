f9d889d18c3961fc19b0ba9785df9cfec0d88c65
Etienne de V <edevalmont@gmail.com>
2025-05-20 11:36:10 +0200

    models users

--- models/users.js
+++ models/users.js
@@ -1,7 +1,28 @@
 const mongoose = require("mongoose");
 
+const premiumDataSchema = new mongoose.Schema({
+  submissionDate: { type: Date, required: true },
+  expirationDate: { type: Date, required: true },
+  facture: { type: String, required: true },
+});
+
+const preferencesSchema = new mongoose.Schema({
+  newsletterSubmission: { type: Boolean, required: true, default: false },
+});
+
+const achievementsSchema = new mongoose.Schema({
+  oneGame: { type: Boolean, default: false },
+  fiveGames: { type: Boolean, default: false },
+  hundredGames: { type: Boolean, default: false },
+  oneFriend: { type: Boolean, default: false },
+  fiveFriends: { type: Boolean, default: false },
+});
 const usersSchema = mongoose.Schema({
-  firstname: {
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
     type: String,
     required: true,
   },
@@ -9,7 +30,12 @@ const usersSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  password: {
+  email: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: { type: Date },
+  hash: {
     type: String,
     required: true,
   },
@@ -17,6 +43,22 @@ const usersSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    type: [String],
+    required: true,
+    default: "guest",
+  },
+  partsPlayed: { type: [Types.ObjectId], ref: "parts", default: [] },
+  friends: { type: [Types.ObjectId], ref: "users", default: [] },
+  premiumeData: premiumDataSchema,
 });
 
 const usersModel = mongoose.model("users", usersSchema);
