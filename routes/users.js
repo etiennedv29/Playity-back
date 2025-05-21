25c5bb85dd812866c9b26c19836f054d050d5123
Etienne de V <edevalmont@gmail.com>
2025-05-21 17:24:09 +0200

    route userbytoken deleted

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-const { login, register,findUserByToken } = require("../controllers/users");
+const { login, register } = require("../controllers/users");
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/:token",findUserByToken)
+
 
 module.exports = router;
