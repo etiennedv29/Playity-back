49f3f7881eb8d45c1345bd742041b0e3a22c84b8
Etienne de V <edevalmont@gmail.com>
2025-05-26 14:28:32 +0200

    automatic piece down and rotate + emit and receive

--- sockets/index.js
+++ sockets/index.js
@@ -1,6 +1,7 @@
 const registerLobbyHandlers = require("./lobby");
 const {spawnPiece} = require("./multitris");
 const {gameStart} = require("./multitris");
+const {communicateMovingPieces}= require("./multitris")
 const User = require("../models/users");
 
 module.exports = function registerSocketHandlers(io) {
@@ -14,5 +15,6 @@ module.exports = function registerSocketHandlers(io) {
     registerLobbyHandlers(io, socket);
     spawnPiece(io, socket);
     gameStart(io, socket);
+    communicateMovingPieces(io,socket)
   });
 };
